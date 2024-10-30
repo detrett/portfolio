17a808eee9b388440d8640d92ec009442a53a38a
Alan Blanco <66321582+detrett@users.noreply.github.com>
2024-10-30 12:23:31 +0100

    add dropdown menu, dark theme button functionality
    add contact info and footer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import "./styles.css";
+import './styles.css';
+import { Dropdown } from '@detrett/dropdown-menu';
+
+const btnToggleTheme = document.querySelector('.btn-toggle-theme');
+
+btnToggleTheme.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme');
+})
