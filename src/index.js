dd9be62485c706ddc9d0f5d534de9c8ebda3b392
Alan Blanco <66321582+detrett@users.noreply.github.com>
2024-10-30 12:44:43 +0100

    add theme icon toggle

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import './styles.css';
 import { Dropdown } from '@detrett/dropdown-menu';
 
 const btnToggleTheme = document.querySelector('.btn-toggle-theme');
+const themeIcon = document.querySelector('.theme-icon');
 
 btnToggleTheme.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme');
+  themeIcon.classList.toggle('dark-theme');
 })
+
