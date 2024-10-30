9b465bff930ff43d3d81bd4dc62b2c9f6a891a91
Alan Blanco <66321582+detrett@users.noreply.github.com>
2024-10-30 16:14:57 +0100

    feat: add icon animation for toggling theme

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import './styles.css';
 import { Dropdown } from './dropdown';
+import sunIcon from './icons/sun2.svg';
+import moonIcon from './icons/moon1.svg';
 
 const btnToggleTheme = document.querySelector('.btn-toggle-theme');
 const themeIcon = document.querySelector('.theme-icon');
 
 btnToggleTheme.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme');
-  themeIcon.classList.toggle('dark-theme');
+
+  const currentIcon = window.getComputedStyle(themeIcon).backgroundImage;
+  
+  themeIcon.classList.remove('enter');
+  themeIcon.classList.add('leave');
+
+  if(currentIcon.includes(sunIcon)) {
+    setTimeout(() => {
+      themeIcon.classList.remove('leave');
+      themeIcon.classList.add('enter');
+      themeIcon.style.backgroundImage = `url(${moonIcon})`;
+    }, 500)
+  } else {
+    setTimeout(() => {
+      themeIcon.classList.remove('leave');
+      themeIcon.classList.add('enter');
+      themeIcon.style.backgroundImage = `url(${sunIcon})`;
+    }, 500)
+  }
 })
 
 // Make the navigation list into a dropdown menu, only on smaller devices.
