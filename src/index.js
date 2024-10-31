3e2c35305a69736f227366a8e9fa884e1fba7905
Alan Blanco <66321582+detrett@users.noreply.github.com>
2024-10-31 17:35:11 +0100

    feat: add dark theme colors

--- src/index.js
+++ src/index.js
@@ -1,46 +1,52 @@
-import './styles.css';
-import { Dropdown } from './dropdown';
-import sunIcon from './icons/sun2.svg';
-import moonIcon from './icons/moon1.svg';
+import "./styles.css";
+import { Dropdown } from "./dropdown";
+import sunIcon from "./icons/sun2.svg";
+import moonIcon from "./icons/moon1.svg";
 
-const btnToggleTheme = document.querySelector('.btn-toggle-theme');
-const themeIcon = document.querySelector('.theme-icon');
+const btnToggleTheme = document.querySelector(".btn-toggle-theme");
+const themeIcon = document.querySelector(".theme-icon");
+const langIcon = document.querySelector(".lang-icon");
+const highlight = document.querySelector(".highlight");
 
-btnToggleTheme.addEventListener('click', () => {
-  document.body.classList.toggle('dark-theme');
+
+btnToggleTheme.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  themeIcon.classList.toggle("dark-theme");
+  langIcon.classList.toggle("dark-theme");
+  highlight.classList.toggle('dark-theme');
 
   const currentIcon = window.getComputedStyle(themeIcon).backgroundImage;
 
-  themeIcon.classList.remove('enter');
-  themeIcon.classList.add('leave');
+  themeIcon.classList.remove("enter");
+  themeIcon.classList.add("leave");
 
-  if(currentIcon.includes(sunIcon)) {
+  if (currentIcon.includes(sunIcon)) {
     setTimeout(() => {
-      themeIcon.classList.remove('leave');
-      themeIcon.classList.add('enter');
+      themeIcon.classList.remove("leave");
+      themeIcon.classList.add("enter");
       themeIcon.style.backgroundImage = `url(${moonIcon})`;
-    }, 500)
+    }, 500);
   } else {
     setTimeout(() => {
-      themeIcon.classList.remove('leave');
-      themeIcon.classList.add('enter');
+      themeIcon.classList.remove("leave");
+      themeIcon.classList.add("enter");
       themeIcon.style.backgroundImage = `url(${sunIcon})`;
-    }, 500)
+    }, 500);
   }
-})
+});
 
 // Make the navigation list into a dropdown menu, only on smaller devices.
-const navList = document.querySelector('.nav-list');
-const mediaQuery = window.matchMedia('(max-width: 1000px)')
+const navList = document.querySelector(".nav-list");
+const mediaQuery = window.matchMedia("(max-width: 1000px)");
 
-const checkMediaForNav = function() {
-  if(mediaQuery.matches) {
-    navList.classList.add('dropdown-menu');
+const checkMediaForNav = function () {
+  if (mediaQuery.matches) {
+    navList.classList.add("dropdown-menu");
   } else {
-    navList.classList.remove('dropdown-menu');
+    navList.classList.remove("dropdown-menu");
   }
-}
+};
 
 checkMediaForNav();
 
-mediaQuery.addEventListener('change', checkMediaForNav)
+mediaQuery.addEventListener("change", checkMediaForNav);
